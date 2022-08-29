0c9e8a7af6cf68d64c2a0c0f053d99fab297069a
LucianoRomero1 <Lucianooromero1@gmail.com>
2022-08-29 10:40:16 -0300

    proyecto de peliculas, proyecto 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="layout">
+      <header className="header">
+        <div className="logo">
+          <div className="play"></div>
+        </div>
+
+        <h1>My movies</h1>
       </header>
+
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <section id="content" className="content">
+        <article className="movie-item">
+          <h3 className="title">Dev Web</h3>
+          <p className="description">Luciano Romero</p>
+
+          <button className="edit">Edit</button>
+          <button className="delete">Delete</button>
+        </article>
+
+        <article className="movie-item">
+          <h3 className="title">Dev Web</h3>
+          <p className="description">Luciano Romero</p>
+
+          <button className="edit">Edit</button>
+          <button className="delete">Delete</button>
+        </article>
+
+        <article className="movie-item">
+          <h3 className="title">Dev Web</h3>
+          <p className="description">Luciano Romero</p>
+
+          <button className="edit">Edit</button>
+          <button className="delete">Delete</button>
+        </article>
+
+        <article className="movie-item">
+          <h3 className="title">Dev Web</h3>
+          <p className="description">Luciano Romero</p>
+
+          <button className="edit">Edit</button>
+          <button className="delete">Delete</button>
+        </article>
+      </section>
+
+      <aside className="lateral">
+        <div className="search">
+          <h3 className="title">Seeker</h3>
+          <form>
+            <input type="text" id="search_field" />
+            <button id="search">Search</button>
+          </form>
+        </div>
+
+        <div className="add">
+          <h3 className="title">Add movie</h3>
+          <form>
+            <input type="text" id="title" placeholder="Title" />
+            <textarea id="description" placeholder="Description"></textarea>
+            <input type="submit" id="save" value="Save" />
+          </form>
+        </div>
+      </aside>
+
+      <footer className="footer">
+        &copy; React JS - <a href="#">Luciano Romero / Dev </a>
+      </footer>
     </div>
   );
 }
